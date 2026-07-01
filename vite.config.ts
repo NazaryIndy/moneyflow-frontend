6486fd58347eb4713482346c99e7e7b30fc5e09a
Прядкина Галина <pryadkina@profilum.ru>
2026-07-01 20:38:27 +0300

    Add ui.shadcn

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,14 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import path from 'path';
+import tailwindcss from '@tailwindcss/vite';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, '../'),
-      '@/shared': path.resolve(__dirname, '../src/shared'),
-      '@/entities': path.resolve(__dirname, '../src/entities'),
-      '@/features': path.resolve(__dirname, '../src/features'),
+      "@": path.resolve(__dirname, "./src"),
     },
   },
 });
